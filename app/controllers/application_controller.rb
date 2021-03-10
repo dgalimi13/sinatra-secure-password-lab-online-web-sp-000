ab5a6125b74311b078203d9d0ce5c751874226ec
danny galimi <dannygalimi13@aim.com>
2021-03-10 01:10:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -17,6 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    binding.pry
     if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else
