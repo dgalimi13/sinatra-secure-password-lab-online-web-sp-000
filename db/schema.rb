e44455b19c88dc4709237c31a337e9c302f90494
danny galimi <dannygalimi13@aim.com>
2021-03-10 01:37:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20210310011105) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
